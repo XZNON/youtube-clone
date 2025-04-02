aed338c2c0752e0bb6fc91c100ffd06ffda9f47d
XZNONz <shivaliksingh39@gmail.com>
2025-04-02 23:40:39 +0530

    add subscription model

---
+++ src/modles/subcriptions.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subcriber: {
+      type: Schema.Types.ObjectId, //user who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId, //one to whome the user is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
